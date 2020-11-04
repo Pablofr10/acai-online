b6a6c0b91fdb2a6f8f6f7ad64b3fef1575f8d0e7
Pablo Rafael Oliveira de Araújo <pabloweb@outlook.com>
2020-11-04 20:17:22 -0300

    Adicionando Axios e criando serviço.

---
+++ src/services/services.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const axiosService = axios.create({
+  baseURL: 'http://localhost:5000/api/',
+});
+
+export const api = {
+  get(endpoint: string) {
+    return axiosService.get(endpoint);
+  },
+  post(endpoint: string, body: any) {
+    return axiosService.post(endpoint);
+  },
+  delete(endpoint: string) {
+    return axiosService.delete(endpoint);
+  },
+  put(endpoint: string) {
+    return axiosService.put(endpoint);
+  },
+};
