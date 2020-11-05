f74f1b87c74ff623b1390ce8566dac2deb75ebb4
Pablo Rafael Oliveira de Araújo <pabloweb@outlook.com>
2020-11-04 21:40:57 -0300

    Adicionando dialógo.

--- src/components/CardProduto.vue
+++ src/components/CardProduto.vue
@@ -11,11 +11,35 @@
       <v-card-subtitle> {{ produto.descricao }}</v-card-subtitle>
 
       <v-card-actions>
-        <v-btn color="orange lighten-2" text> Explore </v-btn>
+        <v-btn color="primary" @click="dialog = true" fab small dark>
+          <v-icon>mdi-eye</v-icon>
+        </v-btn>
+        <v-btn color="primary" fab small dark>
+          <v-icon>mdi-pencil</v-icon>
+        </v-btn>
 
         <v-spacer></v-spacer>
+        <v-btn color="error" fab small dark>
+          <v-icon>mdi-trash-can-outline</v-icon>
+        </v-btn>
       </v-card-actions>
     </v-card>
+    <v-row justify="center">
+      <v-dialog v-model="dialog" persistent max-width="500">
+        <v-card>
+          <v-card-title class="headline">
+            {{ produto.nome }}
+          </v-card-title>
+          <v-card-text> {{ produto.descricao }}</v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="silver darken-1" text @click="dialog = false">
+              Fechar
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-row>
   </div>
 </template>
 
@@ -27,6 +51,7 @@ import { Component, Vue, Prop } from "vue-property-decorator";
 export default class extends Vue {
   @Prop() produto!: Produto;
   show = false;
+  dialog = false;
 }
 </script>
 
