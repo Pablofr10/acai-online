aaa96c8a8a335713332971803fa3406293a55288
Pablo Rafael Oliveira de Araújo <pabloweb@outlook.com>
2020-11-06 22:12:51 -0300

    Adicionando array de pedidos.

--- src/models/Produto.ts
+++ src/models/Produto.ts
@@ -1,7 +1,10 @@
+import { Categoria } from './Categoria';
+
 export interface Produto {
   nome: string;
   preco: number;
   descricao: string;
   imageUrl: string;
   isDisponivel: boolean;
+  categoria: Categoria[];
 }
--- src/views/Produtos.vue
+++ src/views/Produtos.vue
@@ -21,10 +21,15 @@
           :loading="!produtos.length"
           loading-text="Carregando..."
           :search="filtro"
+          :footer-props="{
+            showFirstLastPage: true,
+            itemsPerPageText: 'Total de items por página',
+            itemsPerPageOptions: [5, 10, 15],
+          }"
         >
           <template v-slot:top>
             <v-toolbar flat>
-              <v-toolbar-title>My CRUD</v-toolbar-title>
+              <v-toolbar-title>Produtos</v-toolbar-title>
               <v-divider class="mx-4" inset vertical></v-divider>
               <v-spacer></v-spacer>
               <v-dialog v-model="dialog" max-width="500px">
@@ -36,7 +41,7 @@
                     v-bind="attrs"
                     v-on="on"
                   >
-                    New Item
+                    Novo Produto
                   </v-btn>
                 </template>
                 <v-card>
@@ -50,14 +55,32 @@
                         <v-col cols="12" sm="6" md="4">
                           <v-text-field
                             v-model="editedItem.nome"
-                            label="Dessert nome"
+                            label="Nome"
                           ></v-text-field>
                         </v-col>
                         <v-col cols="12" sm="6" md="4">
                           <v-text-field
-                            v-model="editedItem.isDisponivel"
-                            label="Calories"
+                            v-model="editedItem.descricao"
+                            label="Descrição"
                           ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-text-field
+                            v-model="editedItem.preco"
+                            label="Preço"
+                          ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="12" md="12">
+                          <v-select
+                            v-model="editedItem.categoria"
+                            :items="categorias"
+                            item-text="nome"
+                            item-value="id"
+                            attach
+                            chips
+                            label="Chips"
+                            multiple
+                          ></v-select>
                         </v-col>
                       </v-row>
                     </v-container>
@@ -65,19 +88,25 @@
 
                   <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn color="blue darken-1" text> Cancel </v-btn>
-                    <v-btn color="blue darken-1" text> Save </v-btn>
+                    <v-btn color="blue darken-1" text @click="close">
+                      Cancelar
+                    </v-btn>
+                    <v-btn color="blue darken-1" text @click="salvarProduto">
+                      Salvar
+                    </v-btn>
                   </v-card-actions>
                 </v-card>
               </v-dialog>
               <v-dialog v-model="dialogDelete" max-width="500px">
                 <v-card>
                   <v-card-title class="headline"
-                    >Are you sure you want to delete this item?</v-card-title
+                    >Tem certeza que deseja excluí esse produto?</v-card-title
                   >
                   <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn color="blue darken-1" text>Cancel</v-btn>
+                    <v-btn color="blue darken-1" text @click="close"
+                      >Cancel</v-btn
+                    >
                     <v-btn color="blue darken-1" text>OK</v-btn>
                     <v-spacer></v-spacer>
                   </v-card-actions>
@@ -95,7 +124,7 @@
             <v-btn color="primary" @click="initialize"> Reset </v-btn>
           </template>
           <template v-slot:item.isDisponivel="{ item }">
-            <v-chip color="blue" dark>
+            <v-chip :color="item.isDisponivel ? 'success' : 'orange'" dark>
               {{ item.isDisponivel ? "Disponível" : "Indisponível" }}
             </v-chip>
           </template>
@@ -138,13 +167,23 @@ export default class Produtos extends Vue {
     { text: "Actions", value: "actions", sortable: false },
   ];
 
+  categorias = [
+    { nome: "Picolé", id: 1 },
+    { nome: "Açai", id: 2 },
+    { nome: "Sorvete", id: 3 },
+  ];
+
   get formTitle() {
-    return this.editedIndex === -1 ? "New Item" : "Edit Item";
+    return this.editedIndex === -1 ? "Novo Produto" : "Editar Produto";
   }
 
-  editedItem = {
+  editedItem: Produto = {
     nome: "",
-    isDisponivel: "",
+    isDisponivel: false,
+    preco: 0,
+    descricao: "",
+    imageUrl: "",
+    categoria: [],
   };
   defaultItem = {
     nome: "",
@@ -156,8 +195,40 @@ export default class Produtos extends Vue {
     this.produtos = response.data;
   }
 
+  async adicionarPedido() {
+    const response = await api
+      .post("produto", this.editedItem)
+      .catch((err) => console.log(err));
+
+    this.getPedidos();
+  }
+
   created() {
     this.getPedidos();
+  }
+
+  close() {
+    this.dialog = false;
+    this.$nextTick(() => {
+      this.editedItem = Object.assign({}, null);
+      this.editedIndex = -1;
+    });
+  }
+
+  editItem(item) {
+    console.log(item);
+    this.editedIndex = this.produtos.indexOf(item);
+    this.editedItem = Object.assign({}, item);
+    this.dialog = true;
+  }
+  salvarProduto() {
+    console.log(this.editedItem);
+    if (this.editedIndex > -1) {
+      Object.assign(this.produtos[this.editedIndex], this.editedItem);
+    } else {
+      this.adicionarPedido();
+    }
+    this.close();
   }
 }
 </script>
